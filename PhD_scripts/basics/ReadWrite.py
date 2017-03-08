e3cecaef21f23144e25f5b7ddf33b63387e6fcfb
GaillacR <romaingaillac@hotmail.com>
2017-03-08 12:25:08 +0100

    Turning lists to numpy arrays

--- PhD_scripts/basics/ReadWrite.py
+++ PhD_scripts/basics/ReadWrite.py
@@ -86,7 +86,25 @@ def readFromDistanceFile(filename, nconf):
       for at in range(nconf):
         [t, d] = f.readline().split()
         snap.append(float(d))
-  return np.array(traj)
+  return traj
+
+def readFromDistanceFile2(filename, nconf):
+  traj = np.array([])
+  f = open (filename, "r")
+  while True:
+    last_pos = f.tell()
+    line = f.readline()
+    if (len(line) == 0): break
+    if (len(line)>2):
+      snap = np.array([])
+      np.append(traj,snap)
+      f.seek(last_pos)
+      for at in range(nconf):
+        [t, d] = f.readline().split()
+        np.append(snap,float(d))
+  return traj
+
+
 
 def nconfFromDist(filename):
   foo = 0
